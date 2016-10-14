ac538f690ded2eaf8ec205b1be6357c78d3ef3dd
Andres Quast <quast@hbz-nrw.de>
2016-10-14 17:39:35 +0200

    initial commit

---
+++ src/main/java/de/nrw/hbz/dns/SipReplicationController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.nrw.hbz.dns;
+
+/**
+ * @author aquast
+ *
+ */
+public class SipReplicationController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
